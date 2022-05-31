f7ae9309d69d54f5108fc689d5e003e2fdfdd104
Vitalii Latysh <vitaliylatush@gmail.com>
2022-05-31 16:22:00 +0300

    Running rhosak.java as jbang (#4)
    run rhoasak.java command via jbang. No command passed. Login by default

---
+++ src/main/java/com/redhat/rhosak/RhoasTokens.java
@@ -0,0 +1,18 @@
+package com.redhat.rhosak;
+
+import java.time.Duration;
+
+public class RhoasTokens {
+
+    public String refresh_token;
+    public String access_token;
+    public Long access_expiration;
+    public Long refresh_expiration;
+
+    boolean accessTokenIsValidFor(Duration duration) {
+        return (access_expiration) - duration.toMillis() >= System.currentTimeMillis();
+    }
+    boolean refreshTokenIsValidFor(Duration duration) {
+        return (refresh_expiration) - duration.toMillis() >= System.currentTimeMillis();
+    }
+}
