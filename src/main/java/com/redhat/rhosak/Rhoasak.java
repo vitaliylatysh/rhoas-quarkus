2672ca12ca926c71511a95a4c3ec324d67e47724
Vitalii Latysh <vitaliylatush@gmail.com>
2022-06-03 18:02:34 +0300

    Added service-account list, delete by id commands

--- src/main/java/com/redhat/rhosak/Rhoasak.java
+++ src/main/java/com/redhat/rhosak/Rhoasak.java
@@ -13,8 +13,10 @@ import com.openshift.cloud.api.kas.DefaultApi;
 import com.openshift.cloud.api.kas.SecurityApi;
 import com.openshift.cloud.api.kas.auth.TopicsApi;
 import com.openshift.cloud.api.kas.auth.invoker.auth.OAuth;
+import com.openshift.cloud.api.kas.auth.models.ConfigEntry;
 import com.openshift.cloud.api.kas.auth.models.NewTopicInput;
 import com.openshift.cloud.api.kas.auth.models.Topic;
+import com.openshift.cloud.api.kas.auth.models.TopicSettings;
 import com.openshift.cloud.api.kas.invoker.ApiClient;
 import com.openshift.cloud.api.kas.invoker.ApiException;
 import com.openshift.cloud.api.kas.invoker.Configuration;
@@ -27,12 +29,13 @@ import org.keycloak.adapters.installed.KeycloakInstalled;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import javax.ws.rs.core.GenericType;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 @Command(name = "rhoasak", mixinStandardHelpOptions = true, subcommands = {LoginCommand.class, KafkaCommand.class, ServiceAccountCommand.class})
@@ -48,28 +51,21 @@ public class Rhoasak implements Callable<Integer> {
         int exitCode = new CommandLine(rhoasak).execute(args);
 
         System.exit(exitCode);
-
     }
 }
 
 @Command(name = "login", mixinStandardHelpOptions = true, description = "Login into RHOASAK")
 class LoginCommand implements Callable<Integer> {
 
-    public static final String DEFAULT_CREDENTIALS_FILENAME = "credentials.json";
-    public static final String KEYCLOAK_CONFIG_FILE = "keycloak.json";
-    private static final Duration MIN_TOKEN_VALIDITY = Duration.ofSeconds(30);
-
-
-    final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
     private final KeycloakInstalled keycloak;
 
     public LoginCommand() {
         this.objectMapper = new ObjectMapper();
-        InputStream config = Thread.currentThread().getContextClassLoader().getResourceAsStream(KEYCLOAK_CONFIG_FILE);
-        this.keycloak = new KeycloakInstalled(config);
+        this.keycloak = KeycloakInstance.getKeycloakInstance();
     }
 
-    @CommandLine.Option(names = "-tf, --tokens-file", paramLabel = "tokens-file", description = "File for storing obtained tokens.", defaultValue = DEFAULT_CREDENTIALS_FILENAME)
+    @CommandLine.Option(names = "-tf, --tokens-file", paramLabel = "tokens-file", description = "File for storing obtained tokens.", defaultValue = Files.DEFAULT_CREDENTIALS_FILENAME)
     Path tokensPath;
 
     @Override
@@ -82,25 +78,6 @@ class LoginCommand implements Callable<Integer> {
         storeTokenResponse(keycloak);
         return 0;
     }
-
-//    private String getBearerToken() {
-//        try {
-//            RhoasTokens storedTokens = getStoredTokenResponse();
-//
-//            // ensure token is valid for at least 30 seconds
-//            if (storedTokens != null && storedTokens.accessTokenIsValidFor(MIN_TOKEN_VALIDITY)) {
-//                return storedTokens.access_token;
-//            } else if (storedTokens != null && storedTokens.refreshTokenIsValidFor(MIN_TOKEN_VALIDITY)) {
-//                keycloak.refreshToken(storedTokens.refresh_token);
-//                return keycloak.getTokenString();
-//            } else {
-//                keycloak.loginDesktop();
-//                return keycloak.getTokenString();
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 
     private void storeTokenResponse(KeycloakInstalled keycloak) throws IOException {
         RhoasTokens rhoasTokens = new RhoasTokens();
@@ -223,36 +200,87 @@ class KafkaTopicCommand implements Callable<Integer> {
 @Command(name = "create", mixinStandardHelpOptions = true)
 class KafkaTopicCreateCommand implements Callable<Integer> {
 
+    private final ObjectMapper objectMapper;
+    private final com.openshift.cloud.api.kas.auth.invoker.ApiClient apiInstanceClient;
+    private final ApiClient apiManagementClient;
     private final TopicsApi apiInstanceTopic;
+    private final DefaultApi managementApi;
 
     public KafkaTopicCreateCommand() {
-        this.apiInstanceTopic = new TopicsApi();
+        this.objectMapper = new ObjectMapper();
+        this.apiInstanceClient = KafkaInstanceClient.getKafkaInstanceAPIClient();
+        this.apiManagementClient = KafkaManagementClient.getKafkaManagementAPIClientInstance();
+        this.apiInstanceTopic = new TopicsApi(apiInstanceClient);
+        this.managementApi = new DefaultApi(apiManagementClient);
     }
 
-    @CommandLine.Option(names = "--name", paramLabel = "string", description = "Topic name")
+    @CommandLine.Option(names = "--name", paramLabel = "string", required = true, description = "Topic name")
     String topicName;
 
     @Override
     public Integer call() {
-        if (Objects.nonNull(topicName)) {
+        try {
             System.out.println(createInstanceTopic(topicName));
+        } catch (com.openshift.cloud.api.kas.auth.invoker.ApiException e) {
+            throw new RuntimeException(e);
         }
         return 0;
     }
 
-    private Topic createInstanceTopic(String topicName) {
-        NewTopicInput topicInput = new NewTopicInput();
-        topicInput.setName(topicName);
+    private Topic createInstanceTopic(String topicName) throws com.openshift.cloud.api.kas.auth.invoker.ApiException {
+        NewTopicInput topicInput = getTopicInput(topicName);
+        String serverUrl = getServerUrl();
 
         try {
-            return apiInstanceTopic.createTopic(topicInput);
+            apiInstanceTopic.getApiClient().setBasePath(serverUrl);
+            apiInstanceTopic.getApiClient().setAccessToken(rhosakApiToken());
+            Topic topic = apiInstanceTopic.createTopic(topicInput);
+            System.out.println(">>> topic: " + topic);
+            return topic;
         } catch (com.openshift.cloud.api.kas.auth.invoker.ApiException e) {
-            throw new RuntimeException(e.getMessage());
+            if (e.getCode() == 401 || e.getCode() == 403) {
+
+                //refresh
+
+            } else {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+
+        return null;
+    }
+
+    private String getServerUrl() {
+        try {
+            return  "https://admin-server-" + managementApi.getKafkas(null, null, null, null).getItems().get(0).getBootstrapServerHost();
+        } catch (ApiException e) {
+            throw new RuntimeException("Cannot get kafka url", e.getCause());
+        }
+    }
+
+    private NewTopicInput getTopicInput(String topicName) {
+        NewTopicInput topicInput = new NewTopicInput();
+        topicInput.setName(topicName);
+        TopicSettings ts = new TopicSettings();
+        List<ConfigEntry> list = new ArrayList<>();
+        ts.setConfig(list);
+        ts.setNumPartitions(1);
+        topicInput.setSettings(ts);
+        return topicInput;
+    }
+
+    private String rhosakApiToken() {
+        try {
+            RhoasTokens tokens = objectMapper.readValue(Path.of(Files.RHOSAK_API_CREDS_FILE_NAME + ".json").toFile(), RhoasTokens.class);
+            return tokens.access_token;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
 
-@Command(name = "service-account", mixinStandardHelpOptions = true, subcommands = {ServiceAccountCreateCommand.class, ServiceAccountListCommand.class, ServiceAccountResetCredentialsCommand.class})
+@Command(name = "service-account", mixinStandardHelpOptions = true, subcommands = {ServiceAccountCreateCommand.class, ServiceAccountListCommand.class, ServiceAccountResetCredentialsCommand.class, ServiceAccountDeleteCommand.class})
 class ServiceAccountCommand implements Callable<Integer> {
 
     @Override
@@ -263,13 +291,13 @@ class ServiceAccountCommand implements Callable<Integer> {
 
 @Command(name = "create", mixinStandardHelpOptions = true)
 class ServiceAccountCreateCommand implements Callable<Integer> {
-
-    private static final String SA_FILE_NAME = System.getProperty("user.dir") + File.separator + "rhosak-sa";
     private final ObjectMapper objectMapper;
+    private final com.openshift.cloud.api.kas.auth.invoker.ApiClient kafkaAPIInstanceClient;
     private final SecurityApi securityAPI;
 
     public ServiceAccountCreateCommand() {
         this.objectMapper = new ObjectMapper();
+        this.kafkaAPIInstanceClient = KafkaInstanceClient.getKafkaInstanceAPIClient();
         this.securityAPI = new SecurityApi(KafkaManagementClient.getKafkaManagementAPIClientInstance());
     }
 
@@ -306,17 +334,85 @@ class ServiceAccountCreateCommand implements Callable<Integer> {
     }
 
     private void saveServiceAccountToFile(ServiceAccount serviceAccount) throws IOException {
-        Path saFile = Path.of(SA_FILE_NAME + "." + fileFormat);
+        Path saFile = Path.of(Files.SA_FILE_NAME + "." + fileFormat);
         serviceAccount.setCreatedAt(null); // otherwise .rhosak-sa file will be broken
         objectMapper.writeValue(saFile.toFile(), serviceAccount);
+
+        String clientId = serviceAccount.getClientId();
+        String clientSecret = serviceAccount.getClientSecret();
+
+        Map<String, Object> formParametersMap = new HashMap<>() {{
+            put("grant_type", "client_credentials");
+            put("client_id", clientId);
+            put("client_secret", clientSecret);
+            put("scope", "openid");
+        }};
+        String acceptString = "application/json";
+        String contentTypeString = "application/x-www-form-urlencoded";
+        GenericType<Map<String, String>> returnTypeClass = new GenericType<>() {
+        };
+        String URL = "/auth/realms/rhoas/protocol/openid-connect/token";
+        try {
+            Map<String, String> res = kafkaAPIInstanceClient.invokeAPI(URL,
+                    "POST",
+                    null,
+                    null,
+                    new HashMap<>(),
+                    new HashMap<>(),
+                    formParametersMap,
+                    acceptString,
+                    contentTypeString,
+                    new String[]{"Bearer"},
+                    returnTypeClass
+            );
+
+            Path apiTokensFile = Path.of(Files.RHOSAK_API_CREDS_FILE_NAME + "." + fileFormat);
+            RhoasTokens tokens = new RhoasTokens();
+            tokens.access_token = res.get("access_token");
+            objectMapper.writeValue(apiTokensFile.toFile(), tokens);
+        } catch (com.openshift.cloud.api.kas.auth.invoker.ApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
 @Command(name = "list", mixinStandardHelpOptions = true)
 class ServiceAccountListCommand implements Callable<Integer> {
 
+    private final SecurityApi securityAPI;
+
+    public ServiceAccountListCommand(){
+        this.securityAPI = new SecurityApi(KafkaManagementClient.getKafkaManagementAPIClientInstance());
+    }
     @Override
     public Integer call() {
+        try {
+            System.out.println(securityAPI.getServiceAccounts(null).getItems());
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
+    }
+}
+
+@Command(name = "delete", mixinStandardHelpOptions = true)
+class ServiceAccountDeleteCommand implements Callable<Integer> {
+
+    private final SecurityApi securityAPI;
+
+    @CommandLine.Option(names = "--id", paramLabel = "string", required = true, description = "The unique ID of the service account to delete")
+    String name;
+
+    public ServiceAccountDeleteCommand(){
+        this.securityAPI = new SecurityApi(KafkaManagementClient.getKafkaManagementAPIClientInstance());
+    }
+    @Override
+    public Integer call() {
+        try {
+            securityAPI.deleteServiceAccountById(name);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
         return 0;
     }
 }
@@ -333,17 +429,18 @@ class ServiceAccountResetCredentialsCommand implements Callable<Integer> {
 class KafkaManagementClient {
 
     private static final String API_CLIENT_BASE_PATH = "https://api.openshift.com";
+    private static final Duration MIN_TOKEN_VALIDITY = Duration.ofSeconds(30);
     private static ApiClient kafkaManagementAPIClientInstance;
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final KeycloakInstalled keycloak = KeycloakInstance.getKeycloakInstance();
 
-    private KafkaManagementClient() {
-    }
+    private KafkaManagementClient() {}
 
     public static ApiClient getKafkaManagementAPIClientInstance() {
         if (kafkaManagementAPIClientInstance == null) {
             kafkaManagementAPIClientInstance = Configuration.getDefaultApiClient();
             kafkaManagementAPIClientInstance.setBasePath(API_CLIENT_BASE_PATH);
-            String tokenString = getStoredTokenResponse().access_token;
+            String tokenString = getBearerToken();
 
             // Configure HTTP bearer authorization: Bearer
             HttpBearerAuth bearer = (HttpBearerAuth) kafkaManagementAPIClientInstance.getAuthentication("Bearer");
@@ -355,15 +452,33 @@ class KafkaManagementClient {
     private static RhoasTokens getStoredTokenResponse() {
         try {
             return objectMapper.readValue(
-                    Path.of(LoginCommand.DEFAULT_CREDENTIALS_FILENAME).toFile(),
+                    Path.of(Files.DEFAULT_CREDENTIALS_FILENAME).toFile(),
                     RhoasTokens.class
             );
         } catch (Exception e) {
             return null;
         }
     }
-}
 
+    public static String getBearerToken() {
+        try {
+            RhoasTokens storedTokens = getStoredTokenResponse();
+
+            // ensure token is valid for at least 30 seconds
+            if (storedTokens != null && storedTokens.accessTokenIsValidFor(MIN_TOKEN_VALIDITY)) {
+                return storedTokens.access_token;
+            } else if (storedTokens != null && storedTokens.refreshTokenIsValidFor(MIN_TOKEN_VALIDITY)) {
+                keycloak.refreshToken(storedTokens.refresh_token);
+                return keycloak.getTokenString();
+            } else {
+                keycloak.loginDesktop();
+                return keycloak.getTokenString();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 class KafkaInstanceClient {
 
     private static final String API_INSTANCE_CLIENT_BASE_PATH = "https://identity.api.openshift.com";
@@ -377,26 +492,28 @@ class KafkaInstanceClient {
         if (kafkaInstanceAPIClient == null) {
             kafkaInstanceAPIClient = com.openshift.cloud.api.kas.auth.invoker.Configuration.getDefaultApiClient();
             kafkaInstanceAPIClient.setBasePath(API_INSTANCE_CLIENT_BASE_PATH);
-            String tokenString = getStoredTokenResponse().access_token;
+            String tokenString = KafkaManagementClient.getBearerToken();
 
             OAuth bearer = (OAuth) kafkaInstanceAPIClient.getAuthentication("Bearer");
             bearer.setAccessToken(tokenString);
         }
         return kafkaInstanceAPIClient;
     }
-
-    private static RhoasTokens getStoredTokenResponse() {
-        try {
-            return objectMapper.readValue(
-                    Path.of(LoginCommand.DEFAULT_CREDENTIALS_FILENAME).toFile(),
-                    RhoasTokens.class
-            );
-        } catch (Exception e) {
-            return null;
-        }
-    }
 }
 
+class KeycloakInstance {
+    private static KeycloakInstalled keycloak;
+
+    private KeycloakInstance(){}
+
+    public static KeycloakInstalled getKeycloakInstance() {
+        if (keycloak == null) {
+            InputStream config = Thread.currentThread().getContextClassLoader().getResourceAsStream(Files.KEYCLOAK_CONFIG_FILE);
+            keycloak = new KeycloakInstalled(config);
+        }
+        return keycloak;
+    }
+}
 class RhoasTokens {
 
     public String refresh_token;
@@ -411,4 +528,11 @@ class RhoasTokens {
     boolean refreshTokenIsValidFor(Duration duration) {
         return (refresh_expiration) - duration.toMillis() >= System.currentTimeMillis();
     }
+}
+
+class Files{
+    public static final String KEYCLOAK_CONFIG_FILE = "keycloak.json";
+    public static final String DEFAULT_CREDENTIALS_FILENAME = "credentials.json";
+    public static final String SA_FILE_NAME = System.getProperty("user.dir") + File.separator + "rhosak-sa";
+    public static final String RHOSAK_API_CREDS_FILE_NAME = System.getProperty("user.dir") + File.separator + "rhosak-api-creds";
 }
